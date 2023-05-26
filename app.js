2eeee223295af938fc601911c4183e3879e51c17
sandeep2630 <katwal.sandeep28@gmail.com>
2023-05-26 11:28:01 +0530

    finised the crud functionality

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ app.get("/hello", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 
 const start = async () => {
+
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`server is listening ata port ${port} ...`));
+    app.listen(port, console.log(`server is listening at port ${port} ...`));
   } catch (error) {
     console.log(error);
+
   }
 };
 start();
--- controller/tasks.js
+++ controller/tasks.js
@@ -2,7 +2,9 @@ const Task = require("../modals/task");
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
+
     console.log(tasks);
+
     res.status(200).json({ tasks });
   } catch (error) {
     res.status(500).json({ error });
@@ -24,8 +26,17 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 const createTask = async (req, res) => {
@@ -41,7 +52,7 @@ const deleteTask = async (req, res) => {
   try {
     const { id: taskId } = req.params;
 
-    const task = Task.deleteOne({ _id: taskId });
+    const task = Task.findOneAndDelete({ _id: taskId });
 
     if (!task) {
       return res.status(404).json({ message: `no item found with ${taskId}` });
